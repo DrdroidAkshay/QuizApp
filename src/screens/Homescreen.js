9fbd164a47cf2e18039f213d3a7097f7ff5052a7
Akshay <akshayyadav04031998@gmail.com>
2021-12-07 21:19:51 +0530

    First Commit. Splash screen setup

---
+++ src/screens/Homescreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({});
+
+export default HomeScreen;
